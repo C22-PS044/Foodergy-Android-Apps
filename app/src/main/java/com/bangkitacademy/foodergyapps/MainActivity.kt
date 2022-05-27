df7df938e5c791bd1ef6a60eaf2733a79e2e02d6
rei <35054638+reiriswa@users.noreply.github.com>
2022-05-27 23:08:54 +0700

    Add files via upload

--- app/src/main/java/com/bangkitacademy/foodergyapps/MainActivity.kt
+++ app/src/main/java/com/bangkitacademy/foodergyapps/MainActivity.kt
@@ -2,10 +2,19 @@ package com.bangkitacademy.foodergyapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mButton = findViewById<View>(R.id.button) as Button
+        //handle onClick
+        mButton.setOnClickListener {
+            //intent to start NewActivity
+            startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
+        }
     }
 }
