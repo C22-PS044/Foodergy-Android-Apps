38e31dbd048e1ea3af715e0201efaaebecc7bc9f
abiyans_ <bagss69@gmail.com>
2022-05-29 02:25:50 +0700

    dashboard activity first

--- app/src/main/java/com/bangkitacademy/foodergyapps/RegisterActivity.kt
+++ app/src/main/java/com/bangkitacademy/foodergyapps/RegisterActivity.kt
@@ -1,10 +1,14 @@
 package com.bangkitacademy.foodergyapps
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Button
+import com.bangkitacademy.foodergyapps.dasboard.DashboardActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,7 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
         setupView()
     }
+
     private fun setupView() {
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -23,5 +28,11 @@ class RegisterActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+        val lanjutkan = findViewById<View>(R.id.lanjutkan) as Button
+        //handle onClick
+        lanjutkan.setOnClickListener {
+            //intent to start NewActivity
+            startActivity(Intent(this@RegisterActivity, DashboardActivity::class.java))
+        }
     }
 }
