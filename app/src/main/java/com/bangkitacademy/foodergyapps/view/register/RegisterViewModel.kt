9327c3266dcf89a270b41341d80bd5943390eb2e
rei <35054638+reiriswa@users.noreply.github.com>
2022-06-05 22:33:13 +0700

    Add files via upload

---
+++ app/src/main/java/com/bangkitacademy/foodergyapps/view/register/RegisterViewModel.kt
@@ -0,0 +1,16 @@
+package com.bangkitacademy.foodergyapps.view.register
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class RegisterViewModel  @Inject constructor(): ViewModel() {
+
+    fun register(){
+
+    }
+}
